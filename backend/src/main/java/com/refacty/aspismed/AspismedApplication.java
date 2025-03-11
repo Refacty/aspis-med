54e8e316f5ff57877672fa58e3bd3ee9798a73cc
CarlossDMC <carlosdaniel.mc@hotmail.com>
2025-03-11 19:54:45 -0300

    initial commit back-end

---
+++ backend/src/main/java/com/refacty/aspismed/AspismedApplication.java
@@ -0,0 +1,13 @@
+package com.refacty.aspismed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspismedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspismedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/refacty/aspismed/AspismedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refacty.aspismed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspismedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

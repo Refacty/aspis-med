fe1792f8ac3c968abcc19a110b743c757b7e556d
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2025-03-28 21:31:36 -0300

    add fetch patients (partial)

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 // lib/api.ts
 export async function loginUser(email: string, password: string): Promise<string> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/login`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
@@ -13,3 +13,22 @@ export async function loginUser(email: string, password: string): Promise<string
     const data = await res.text() // ou .json(), dependendo de como o backend retorna
     return data // supondo que seja apenas o token
 }
+
+export async function fetchPatients() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/patients`)
+    if (!res.ok) throw new Error("Failed to fetch patients")
+    return res.json()
+}
+
+export async function createPatient(patientData: any) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/patients`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(patientData),
+    })
+    if (!res.ok) throw new Error("Failed to create patient")
+    return res.json()
+}
+
